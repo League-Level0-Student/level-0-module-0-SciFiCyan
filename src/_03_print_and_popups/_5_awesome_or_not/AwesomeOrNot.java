d3a5375c0e35eebe4d7a373f0c27b734cb77d6af
root <root@ab246fb9354b>
2021-09-15 03:02:56 +0000

    bloop

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -2,6 +2,8 @@ package _03_print_and_popups._5_awesome_or_not;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
@@ -18,21 +20,28 @@ public static void main(String[] args) {
 	// 3. Print your variable to the console
 	
 		// 4. Get the user to enter something that they think is awesome
-	
+JOptionPane.showInputDialog("Enter something that you think is awesome!");
 		// 5. If your variable is  0
-	
+	if (random == 0) {
+	JOptionPane.showMessageDialog(null, "WHAT YOU ENTERED IS AWESOME!!!");	
+	}
 			// -- tell the user whatever they entered is awesome!
 	
-		// 6. If your variable is  1
 	
 			// -- tell the user whatever they entered is ok.
-	
+	if(random== 1) {
+	JOptionPane.showMessageDialog(null,"WhAt YoU EnTeReD iS oK.");	
+	}
 		// 7. If your variable is  2
-	
+	if(random== 2) {
+		JOptionPane.showMessageDialog(null,"wut you entered is boring ?");
+	}
 			// -- tell the user whatever they entered is boring.
 	
 		// 8. If your variable is  3
-	
+	if (random== 3){
+		JOptionPane.showMessageDialog(null, "!!!!!!!!!BLOOOOOP!!!!!!!!!!");
+	}
 			// -- invent your own message to give to the user (be nice).
 }
 }
